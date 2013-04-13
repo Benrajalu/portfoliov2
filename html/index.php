3921a0934778a3e018f5c98a4ebe59dfcac9978e
unknown <benoit@perpetualmotionmachine.fr>
2013-04-13 11:29:45 +0200

    Enhanced routing

--- html/index.php
+++ html/index.php
@@ -8,12 +8,16 @@
        
        $tab_title=array("index"=>"Intégrateur web à Rennes, Nantes",
                         "projets"=>"Intégrateur web et developpeur front-end",
+                        "bio"=>"CV - Intégrateur web dans l'Ouest de la France",
+                        "cheiguei"=>"Cheguei Paris - Intégration responsive",
                         "404"=>"Cette page n'existe pas !");
        $tab_description=array("index"=>"Benoit Rajalu est un Intégrateur web et developpeur front-end. Vous travaillez à Nantes ou à Rennes ? Faisons connaissance !",
                         "projets"=>"L'intégration et le développement front-end de ces projets ont été réalisés par les mains curieuses et parsemée de sucre de Benoit Rajalu",
+                        "bio"=>"Voici la liste des ingrédients nécéssaire à la fabrication de Benoit Rajalu. Résultats non-garantis.",
+                        "cheiguei"=>"Ce site responsive a été intégré par Benoit Rajalu. Mais toute la gloire revient à The Quest et PMM", 
                         "404"=>"Oups. Cette page n'existe pas. Un lutin est sûrement en cause.");
                     
-       $setPages=array("index", "projets", "bio", "carte");
+       $setPages=array("index", "projets", "bio", "cheiguei");
        
        if(isset($_GET['p']) && !preg_match("/\//i", $_GET['p']))
        {
@@ -27,6 +31,19 @@
                    header("Location: 404.html");
             }                                  
        }
+       elseif(isset($_GET['pr']))
+       {
+            if(in_array($_GET['pr'], $setPages)){
+                  $content=$_GET['pr'] . ".html"; 
+                  $title=$tab_title[$_GET['pr']];
+                  $description=$tab_description[$_GET['pr']];
+                  $path="../";
+            } 
+            else{
+                   header('HTTP/1.0 404 Not Found');
+                   header("Location: 404.html");
+            }  
+       }
        else{
                   $_GET['p']="index";
                   $content="index.html";
@@ -35,7 +52,7 @@
        }
        
 
-       if(!isset($_GET['p']))
+       if(!isset($_GET['p']) && !isset($_GET['pr']))
        {
                   $content="index" . ".html"; 
                   $title=$tab_title["index"];
